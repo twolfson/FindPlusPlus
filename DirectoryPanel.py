9c34e035de186b05fb6ed7325a36d63d8d582c29
Todd Wolfson <todd@twolfson.com>
2013-07-10 23:53:57 -0700

    Added plenty of dev prints

--- DirectoryPanel.py
+++ DirectoryPanel.py
@@ -9,7 +9,7 @@ import sublime_plugin
 SETTINGS_KEY = 'FindPlusPlus'
 
 
-class DirectoryPanel(sublime_plugin.WindowCommand):
+class DirectoryPanelCommand(sublime_plugin.WindowCommand):
     relative_paths = []
     full_torelative_paths = {}
     rel_path_start = 0
@@ -27,6 +27,7 @@ class DirectoryPanel(sublime_plugin.WindowCommand):
         self.cb = cb
 
         # If there is only one directory, return early with it
+        print 'heeey', self.relative_paths
         if len(self.relative_paths) == 1:
             self.selected_dir = self.relative_paths[0]
             self.selected_dir = self.full_torelative_paths[self.selected_dir]
@@ -40,8 +41,8 @@ class DirectoryPanel(sublime_plugin.WindowCommand):
         # Otherwise, attempt to resolve the directory of the current file
         else:
             view = self.window.active_view()
-            self.selected_dir = os.path.dirname(view.file_name())
-            self.complete()
+            # self.selected_dir = os.path.dirname(view.file_name())
+            # self.complete()
 
     def construct_excluded_pattern(self):
         patterns = [pat.replace('|', '\\') for pat in self.get_setting('excluded_dir_patterns')]
@@ -64,33 +65,10 @@ class DirectoryPanel(sublime_plugin.WindowCommand):
         return results
 
     def build_relative_paths(self):
-        folders = self.window.folders()
-        self.relative_paths = []
-        self.full_torelative_paths = {}
-        for path in folders:
-            rootfolders = os.path.split(path)[-1]
-            self.rel_path_start = len(os.path.split(path)[0]) + 1
-            if not self.excluded.search(rootfolders):
-                self.full_torelative_paths[rootfolders] = path
-                self.relative_paths.append(rootfolders)
-
-            for base, dirs, files in os.walk(path):
-                for dir in dirs:
-                    relative_path = os.path.join(base, dir)[self.rel_path_start:]
-                    if not self.excluded.search(relative_path):
-                        self.full_torelative_paths[relative_path] = os.path.join(base, dir)
-                        self.relative_paths.append(relative_path)
+        print 'mah'
 
     def move_current_directory_to_top(self):
-        view = self.window.active_view()
-        if view.file_name():
-            cur_dir = os.path.dirname(view.file_name())[self.rel_path_start:]
-            if cur_dir in self.full_torelative_paths:
-                i = self.relative_paths.index(cur_dir)
-                self.relative_paths.insert(0, self.relative_paths.pop(i))
-            else:
-                self.relative_paths.insert(0, os.path.dirname(view.file_name()))
-        return
+        print 'x'
 
     def dir_selected(self, selected_index):
         if selected_index != -1:
